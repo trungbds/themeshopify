aa2eee74087fd4afffe93998859cacb730c24844
brianne810 <brianne810@gmail.com>
2024-10-02 19:14:37 -0600

    Cập nhật các component và xóa các tệp không cần thiết

--- app/components/custom-components/SearchHeader.tsx
+++ app/components/custom-components/SearchHeader.tsx
@@ -57,7 +57,7 @@ export function SearchHeader(
           >
             <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"> Search </label>
             <div className="relative">
-              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none z-6">
                   <img src={iconsearch} width="20px" height="auto"/>
               </div>
               <input
