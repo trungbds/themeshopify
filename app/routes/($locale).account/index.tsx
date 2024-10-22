b0b5e53f27cca33e274446b8992a20af02cf9448
brianne810 <brianne810@gmail.com>
2024-10-21 19:30:26 -0600

    Cập nhật các component và xóa các tệp không cần thiết

---
+++ app/routes/($locale).account/index.tsx
@@ -0,0 +1,85 @@
+import {json, type LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import {Form, NavLink, Outlet, useLoaderData} from '@remix-run/react';
+import {CUSTOMER_DETAILS_QUERY} from '~/graphql/customer-account/CustomerDetailsQuery';
+
+export function shouldRevalidate() {
+  return true;
+}
+
+export async function loader({context}: LoaderFunctionArgs) {
+  const {data, errors} = await context.customerAccount.query(
+    CUSTOMER_DETAILS_QUERY,
+  );
+
+  if (errors?.length || !data?.customer) {
+    throw new Error('Customer not found');
+  }
+
+  return json(
+    {customer: data.customer},
+    {
+      headers: {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+      },
+    },
+  );
+}
+
+export default function AccountLayout() {
+  const {customer} = useLoaderData<typeof loader>();
+
+  const heading = customer
+    ? customer.firstName
+      ? `Welcome, ${customer.firstName}`
+      : `Welcome to your account.`
+    : 'Account Details';
+
+  return (
+    <div className="account">
+      <h1>{heading}</h1>
+      <AccountMenu />
+      <Outlet context={{customer}} />
+    </div>
+  );
+}
+
+function AccountMenu() {
+  function isActiveStyle({
+    isActive,
+    isPending,
+  }: {
+    isActive: boolean;
+    isPending: boolean;
+  }) {
+    return {
+      fontWeight: isActive ? 'bold' : undefined,
+      color: isPending ? 'grey' : 'black',
+    };
+  }
+
+  return (
+    <nav role="navigation">
+      <NavLink to="/account/orders" style={isActiveStyle}>
+        Orders  
+      </NavLink>
+        | 
+      <NavLink to="/account/profile" style={isActiveStyle}>
+           Profile  
+      </NavLink>
+        | 
+      <NavLink to="/account/addresses" style={isActiveStyle}>
+           Addresses  
+      </NavLink>
+        | 
+      <Logout />
+    </nav>
+  );
+}
+
+function Logout() {
+  return (
+    <Form className="account-logout" method="POST" action="/account/logout">
+      <button type="submit">Sign out</button>
+    </Form>
+  );
+}
