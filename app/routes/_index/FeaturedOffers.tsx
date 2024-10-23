22fb0c93109ae482e8d57fe8ace4fd48c6cbdc3d
brianne810 <brianne810@gmail.com>
2024-10-23 01:29:46 -0600

    Cập nhật các component và xóa các tệp không cần thiết

--- app/routes/_index/FeaturedOffers.tsx
+++ app/routes/_index/FeaturedOffers.tsx
@@ -10,10 +10,10 @@ export default function FeaturedOffers(
         <section className="featured-offers">
             <div className="container">
                 <h2 className="col-span-4 title-selection">Our Featured Offers</h2>
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4 h-full">
+                <div className="grid">
 
                     {collections.map((collection:any) => (
-                        <div className="col-span-1 sm:col-span-1 md:col-span-1 ">
+                        <div className="grid-item ">
                         
                             <Link 
                                 key={collection.id}
